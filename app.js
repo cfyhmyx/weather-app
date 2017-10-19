012fa03cde40ced92d875949e16267fd5ba867bb
cfyhmyx <cfyhmyx@gmail.com>
2017-10-18 22:23:50 -0700

    use google geo api

---
+++ app.js
@@ -0,0 +1,24 @@
+const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            description: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+    geocode.geocodeAdress(argv.a, (errorMessage, results)=>{
+        if(errorMessage) {
+            console.log(errorMessage);
+        } else {
+            console.log(JSON.stringify(results, undefined, 2));
+        }
+    });
